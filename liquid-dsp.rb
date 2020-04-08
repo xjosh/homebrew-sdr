4f4528b59a34f42e200d7886dea163c6fad56211
xjosh <xjosh@users.noreply.github.com>
2020-04-07 23:09:57 -0400

    Added version
    Make error go away

--- liquid-dsp.rb
+++ liquid-dsp.rb
@@ -5,6 +5,7 @@ class LiquidDsp < Formula
   head "https://github.com/jgaeddert/liquid-dsp.git"
   # inspectrum needs a recent version of liquid-dsp and the latest release is from 2012.
   url "https://github.com/jgaeddert/liquid-dsp.git", :revision => "1191179b786703b3af20abf7e1404d91099b335d"
+  version "2012"
 
   depends_on "fftw"
   depends_on "autoconf" => :build
